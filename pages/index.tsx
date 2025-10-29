42e4f4af661fb50312e3b170aa0bad64d65f39b5
davidlinda7 <david@llmedia.info>
2025-10-29 04:59:23 -0400

    Delete pages/index.tsx

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Widgets Project</h1>
-      <p>Routes: /widget-banner, /widget-video, /widget-metrics, /widget-checklist, /widget-upsells</p>
-    </main>
-  );
-}
